22c0cf78dd272aa731b426431e4ae35ea9bfc2a4
elmasomer <elmassomr@gmail.com>
2026-08-06 13:45:07 +0300

    Record değişikliğinden dolayı response kısmına o anki tarihi ekledim

--- Api/Controllers/ChatController.cs
+++ Api/Controllers/ChatController.cs
@@ -1,4 +1,4 @@
-﻿using applicaiton.DTOs;
+﻿using Application.DTOs;
 using Application.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,15 +8,10 @@ namespace Api.Controllers;
 [Route("api/v1/[controller]")] 
 public class ChatController : ControllerBase
 {
-   
     [HttpPost]
-    public IActionResult SendMessage([FromBody] ChatRequestDTO request)
+    public IActionResult SendMessage([FromBody] ChatRequestDto request)
     {
-      
-        var response = new ChatResponseDto
-        {
-            Reply = "Merhaba, ben sahte bir asistanım. Mesajınızı aldım."
-        };
+        var response = new ChatResponseDto("Merhaba, ben sahte bir asistanım. Mesajınızı aldım.", DateTime.UtcNow);
 
         return Ok(response);
     }
