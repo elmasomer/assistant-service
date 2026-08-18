e5f963a4dbe8822f5e282ed39ebf0b9242aa7193
elmasomer <elmassomr@gmail.com>
2026-08-18 13:53:05 +0300

    feat: ChatSession ve ChatMessage veri modelleri ile EF Core migration eklendi

--- Infrastructure/Context/ApplicationDbContext.cs
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -1,11 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace Infrastructure.Contexts
+namespace Infrastructure.Context 
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+        }
+
+      
+        public DbSet<ChatSession> ChatSessions { get; set; }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        
+            modelBuilder.Entity<ChatSession>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired().HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<ChatMessage>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Role).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Content).IsRequired();
+
+                entity.HasOne(m => m.Session)
+                      .WithMany(s => s.Messages)
+                      .HasForeignKey(m => m.ChatSessionId)
+                      .OnDelete(DeleteBehavior.Cascade);
+            });
         }
     }
 }
